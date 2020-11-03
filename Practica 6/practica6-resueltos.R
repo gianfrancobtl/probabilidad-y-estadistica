d8e67b6d21ed34562ecfcb3602146b75ed735119
Gian <gianfrancobtl@gmail.com>
2020-11-03 11:02:36 -0300

    Actualizado

--- Practica 6/practica6-resueltos.R
+++ Practica 6/practica6-resueltos.R
@@ -5,11 +5,30 @@ alfajores <- read.table("alfajores.txt", header = TRUE)
 table(alfajores)
 
 
-# EERCICIO 2
-# EERCICIO 3
-# EERCICIO 4
-# EERCICIO 5
-# EERCICIO 6
-# EERCICIO 7
-# EERCICIO 8
-# EERCICIO 9
+# EJERCICIO 2
+lamparas <- read.csv("lamparas.txt", header = FALSE)
+acumulada <- function(t)
+{
+  # Compara el vector con el número.
+  mean(lamparas[,1] <= t)
+}
+
+acumulada(5)
+acumulada(8)
+
+# Aplica la acumulada a cada elemento de la grilla
+resultado <- sapply(grilla, acumulada)
+
+range(datos[,1])
+grilla <- seq(-5, 120, length = 1000)
+plot(grilla,resultado)
+
+ecdf(datos[,1])
+
+# EJERCICIO 3
+# EJERCICIO 4
+# EJERCICIO 5
+# EJERCICIO 6
+# EJERCICIO 7
+# EJERCICIO 8
+# EJERCICIO 9
