95f19d05cacca46b8576fbee0ace186edb2b7a4b
Gian <gianfrancobtl@gmail.com>
2020-11-03 10:58:38 -0300

    Ejercicios de la práctica 6.

---
+++ Practica 6/practica6-resueltos.R
@@ -0,0 +1,15 @@
+# PRACTICA 6
+
+# EJERCICIO 1
+alfajores <- read.table("alfajores.txt", header = TRUE)
+table(alfajores)
+
+
+# EERCICIO 2
+# EERCICIO 3
+# EERCICIO 4
+# EERCICIO 5
+# EERCICIO 6
+# EERCICIO 7
+# EERCICIO 8
+# EERCICIO 9
